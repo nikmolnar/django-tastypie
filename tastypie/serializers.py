b67c10cb382c017cdcaf4ef8ec02b0ffa4d6beba
Nikolas Stevenson-Molnar <nik.molnar@consbio.org>
2013-12-12 14:16:21 -0800

    Fix for #1099

--- tastypie/serializers.py
+++ tastypie/serializers.py
@@ -140,9 +140,9 @@ class Serializer(object):
 
         Default is ``iso-8601``, which looks like "2010-12-16T03:02:14".
         """
-        data = make_naive(data)
         if self.datetime_formatting == 'rfc-2822':
             return format_datetime(data)
+        data = make_naive(data)
         if self.datetime_formatting == 'iso-8601-strict':
             # Remove microseconds to strictly adhere to iso-8601
             data = data - datetime.timedelta(microseconds = data.microsecond)
